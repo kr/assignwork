b65407ed18a813493aaa725c45e565b1824f3d08
Keith Rarick <kr@xph.us>
2013-08-29 02:07:51 -0700

    init wip

---
+++ assign.go
@@ -0,0 +1,69 @@
+package assignwork
+
+import (
+	"hash/crc64"
+	"math/rand"
+)
+
+var isoTab = crc64.MakeTable(crc64.ISO)
+
+// Pool represents a work pool of Size members.
+// Its methods assign ownership of work items,
+// which are represented by byte sequences.
+type Pool struct {
+	Size int
+}
+
+// Owners returns the list of owners for item, in priority order.
+// The returned slice has length p.Size.
+//
+// If a single owner is needed, use element 0 in the returned slice;
+// if two are needed, use 0 and 1; etc.
+func (p Pool) Owners(item []byte) []int {
+	c := int64(crc64.Checksum(item, isoTab))
+	return rand.New(rand.NewSource(c)).Perm(p.Size)
+}
+
+// OwnersExcluding is like Owners, except it removes from its
+// return value the elements of exclude. (So the length of the
+// returned slice may be less than p.Size.)
+func (p Pool) OwnersExcluding(item []byte, exclude []int) []int {
+	var a []int
+	for _, k := range p.Owners(item) {
+		if !contains(exclude, k) {
+			a = append(a, k)
+		}
+	}
+	return a
+}
+
+// Member represents a member of a work pool.
+type Member struct {
+	ID int // Must be >= 0 and < Pool.Size
+	Pool
+}
+
+// NewMember returns a Member with ID congruent to k mod n,
+// in a Pool of size n.
+func NewMember(k, n int) Member {
+	return Member{(k%n + n) % n, Pool{n}}
+}
+
+// Owns returns whether m is the first owner of item.
+func (m Member) Owns(item []byte) bool {
+	return m.In(m.Owners(item)[:1])
+}
+
+// Owns returns whether m is in owners.
+func (m Member) In(owners []int) bool {
+	return contains(owners, m.ID)
+}
+
+func contains(a []int, i int) bool {
+	for _, n := range a {
+		if n == i {
+			return true
+		}
+	}
+	return false
+}
---
+++ example_test.go
@@ -0,0 +1,23 @@
+package assignwork_test
+
+import (
+	"github.com/kr/assignwork"
+)
+
+const (
+	NDyno          = 5
+	Dyno           = 2
+	RecheckWorkers = 1
+)
+
+var self = assignwork.NewMember(Dyno, NDyno)
+
+func own(s string) bool {
+	return self.Owns([]byte(s))
+}
+
+func shouldRecheck(s, t string) bool {
+	excl := self.Pool.Owners([]byte(s))[:1]
+	owners := self.Pool.OwnersExcluding([]byte(s+t), excl)
+	return self.In(owners[:RecheckWorkers])
+}
